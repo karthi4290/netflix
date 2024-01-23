749c4919f15828f0888c664eb3b0b16d752c0f0a
karthikreddy.mudireddy <karthikreddy.mudireddy@incentsoft.com>
2024-01-23 16:33:42 -0600

    added navigation arrows for slider

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,7 +4,7 @@ import { POSTERPATH_URL } from '../utils/constants'
 const MovieCard = ({ posterPath }) => {
     if(!posterPath) return null;
     return (
-        <div className=" w-36 md:w-48 pr-4">
+        <div className=" w-36 md:w-[220px] p-2 inline-block cursor-pointer hover:scale-105 ease-in-out duration-300">
             <img
                 alt="movieCard"
                 src={POSTERPATH_URL + posterPath} />
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,19 +1,37 @@
-import React from 'react'
-import MovieCard from './MovieCard'
+import React, { useRef } from 'react';
+import MovieCard from './MovieCard';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 const MovieList = ({ title, movies }) => {
-    
+    const sliderRef = useRef(null);
+
+
+    const handleDirection = (direction) => {
+        const slider = sliderRef.current;
+        const scrollAmount = slider.offsetWidth;
+        if (direction === 'left') {
+            slider.scrollLeft -= scrollAmount;
+        } else {
+            slider.scrollLeft += scrollAmount;
+        }
+    };
+
+
     return (
-        <div className="p-6">
-          <h1 className=" text-lg md:text-3xl py-4 text-white">{title}</h1>
-            <div className="flex overflow-x-auto">
-                <div className="flex">
-                    {movies?.map((movie) => (<MovieCard key={movie.id} posterPath={movie.poster_path} />))}
+        <div className="p-6 relative">
+            <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
+
+            <div className="relative">
+                <MdChevronLeft onClick={() => handleDirection('left')} className="opacity-50 cursor-pointer hover:opacity-100 absolute top-1/2 left-0 transform -translate-y-1/2 z-10" size={40} />
+                <div ref={sliderRef} id="slider" className="w-full h-full overflow-x-hidden overscroll-x-scroll scroll whitespace-nowrap scroll-smooth relative">
+                    {movies?.map((movie) => (
+                        <MovieCard key={movie.id} posterPath={movie.poster_path} />
+                    ))}
                 </div>
+                <MdChevronRight onClick={() => handleDirection('right')} className="opacity-50 cursor-pointer hover:opacity-100 absolute top-1/2 right-0 transform -translate-y-1/2 z-10" size={40} />
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default MovieList
+export default MovieList;
